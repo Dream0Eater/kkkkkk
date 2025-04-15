20ad12cb21605d8db71a0ffe1c45b51672b9243f
Dream0Eater <wakilul112@gmail.com>
2025-04-15 13:23:46 +0600

    commititii

---
+++ src/headers.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+class Logger {
+private:
+    Logger() {}
+
+public:
+    static Logger& getInstance() {
+        static Logger instance;
+        return instance;
+    }
+
+    void log(const std::string& message) {
+        std::cout << "[LOG] " << message << std::endl;
+    }
+
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+};
